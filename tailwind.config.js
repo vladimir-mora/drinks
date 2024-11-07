d55b6f7da144fa0276fef0b28021fa9110778f04
vladimir-mora <vladimirescalante75@gmail.com>
2024-11-07 02:24:32 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        header: "url('/bg.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
